c8feb4b51c9577154fb84b6e482483cc78db7d2a
raj0580 <duckdikcow@gmail.com>
2025-09-10 15:04:07 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+// এই ফাইলটিতে আপনার সমস্ত গোপন API Key থাকবে।
+// দয়া করে এখানে আপনার আসল কী (key) গুলো সাবধানে পেস্ট করুন।
+
+const CONFIG = {
+    // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9tQ9a8buiWOMCy8m5ZA7M07WVo5lPigI",
+  authDomain: "doctor-web-test-ca38e.firebaseapp.com",
+  projectId: "doctor-web-test-ca38e",
+  storageBucket: "doctor-web-test-ca38e.firebasestorage.app",
+  messagingSenderId: "167806498685",
+  appId: "1:167806498685:web:177aee8141ae4edb44fe14"
+};
+    // আপনার ImgBB API Key
+    IMGBB_API_KEY: "5090ec8c335078581b53f917f9657083",
+
+    // আপনার Razorpay Key ID (Key Secret নয়)
+    RAZORPAY_KEY_ID: "YOUR_RAZORPAY_KEY_ID"
+};
